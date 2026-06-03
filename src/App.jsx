d0bd144a2c0a62d9a6876f40870f9f6d7002f100
Diego Lario <diego.lario@demoscope.ch>
2026-06-03 13:06:00 +0200

    imports anpassen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useMemo, useState } from "react";
 import FileUpload from "./components/FileUpload";
-import DataConfig from "./components/DataConfig";
-import Dashboard from "./components/Dashboard";
-import { analyseData } from "./utils/fileParser";
 
 function App() {
     const [fileData, setFileData] = useState(null);
